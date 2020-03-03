81aa232e21bdbfc9fce3878b6c21ad04f7915133
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2020-03-02 21:18:31 -0500

    Add files via upload

---
+++ Curso.pl
@@ -0,0 +1,15 @@
+es_estudiante(nombre(kick, corte_1(,,) ,corte_2() ,corte_3( ))).
+es_estudiante(nombre(kick1, corte_1() ,corte_2() ,corte_3( ))).
+es_estudiante(nombre(kick2, corte_1() ,corte_2() ,corte_3( ))).
+es_estudiante(nombre(kick3, corte_1() ,corte_2() ,corte_3( ))).
+es_estudiante(nombre(kick4, corte_1() ,corte_2() ,corte_3( ))).
+es_estudiante(nombre(kick5, corte_1() ,corte_2() ,corte_3( ))).
+es_estudiante(nombre(kick6, corte_1() ,corte_2() ,corte_3( ))).
+es_estudiante(nombre(kick7, corte_1() ,corte_2() ,corte_3( ))).
+es_estudiante(nombre(kick8, corte_1() ,corte_2() ,corte_3( ))).
+es_estudiante(nombre(kick9, corte_1() ,corte_2() ,corte_3( ))).
+
+llega_clase().
+entrega_trabajos().
+examen().
+aprobar().
