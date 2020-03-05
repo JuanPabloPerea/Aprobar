ef46ed36b024904065a7836791bbed78ca4fea77
elmetacross <54289101+elmetacross@users.noreply.github.com>
2020-03-05 00:54:13 -0500

    Add files via upload

---
+++ adivina quien.pl
@@ -0,0 +1,20 @@
+atjuan("ojos negros","cabello castaño","moreno").
+atcamilo("ojos negros","cabello castaño","negro").
+atpablo("ojos verdes","cabello rubio","blanco").
+
+atsebastian("ojos azules","cabello castaño","blanco").
+atnicolas("ojos verdes","cabello rubio","moreno").
+atbrayan("ojos verdes","cabello rubio","blanco").
+
+atalex("ojos azules","cabello castaño","moreno").
+atjose("ojos negros","cabello castaño","negro").
+atalejandro("ojos azules","cabello castaño","negro").
+
+
+tablero(["juan","camilo","pablo"],
+        ["sebastian","nicolas","brayan"],
+        ["alex","jose","alejandro"]).
+
+member("ojos negros", [atjuan]).
+
+write(Tablero).
